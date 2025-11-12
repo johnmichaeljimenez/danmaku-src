39e04a1af52ec68f734940e76377956ead9f2cfe
John <johnmichaeljimenez.thecmd@gmail.com>
2025-11-12 23:32:11 +0800

    Added basic player movement and rendering

--- src/main.c
+++ src/main.c
@@ -8,14 +8,51 @@ int main(void)
 
     SetTargetFPS(60);
 
+    Player player = {0};
+    player.IsAlive = true;
+    player.FireTimer = 0;
+    player.HurtboxSize = (Vector2){64, 64};
+    player.MovementSpeed = 512;
+    player.Position = (Vector2){VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT - 100};
+
     while (!WindowShouldClose())
     {
+        if (player.IsAlive)
+        {
+            Vector2 inputMovement = Vector2Zero();
+
+            if (IsKeyDown(KEY_A))
+                inputMovement.x = -1;
+            else if (IsKeyDown(KEY_D))
+                inputMovement.x = 1;
+                
+            if (IsKeyDown(KEY_W))
+                inputMovement.y = -1;
+            else if (IsKeyDown(KEY_S))
+                inputMovement.y = 1;
+
+            if (Vector2LengthSqr(inputMovement) > 0)
+                inputMovement = Vector2Normalize(inputMovement);
+
+            player.Position.x += inputMovement.x * GetFrameTime() * player.MovementSpeed;
+            player.Position.y += inputMovement.y * GetFrameTime() * player.MovementSpeed;
+
+            player.Position = Vector2Clamp(player.Position, (Vector2){0, 0}, (Vector2){VIRTUAL_WIDTH, VIRTUAL_HEIGHT});
+        }
+
         BeginScreen();
-        ClearBackground(DARKGRAY);
+        {
+            ClearBackground(DARKGRAY);
 
-        
+            DrawRectangle(
+                player.Position.x - player.HurtboxSize.x * 0.5,
+                player.Position.y - player.HurtboxSize.y * 0.5,
+                player.HurtboxSize.x,
+                player.HurtboxSize.y,
+                GREEN);
 
-        EndScreen();
+            EndScreen();
+        }
         DrawScreen();
     }
 
