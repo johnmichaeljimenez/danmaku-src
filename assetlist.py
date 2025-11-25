e61ee23d2310b0862569810fe994bf1472092787
John <johnmichaeljimenez.thecmd@gmail.com>
2025-11-26 00:23:14 +0800

    assetlist.py sfx scanner

--- assetlist.py
+++ assetlist.py
@@ -1,14 +1,20 @@
-
 import os
+from collections import defaultdict
 
 sprite_h = ""
 sprite_c = ""
+sfx_h = ""
+sfx_c = ""
+
 def code_h():
 	return f"""
 #pragma once
 #include "main.h"
 
 {sprite_h}
+
+{sfx_h}
+
 """.strip()
 
 def code_c():
@@ -16,10 +22,17 @@ def code_c():
 #include "gen_assets.h"
 
 {sprite_c}
+
+{sfx_c}
+
 """.strip()
 
+def sanitize(i) -> str:
+	i = i.replace("assets/", "")
+	i, ext = os.path.splitext(i)
+	return i.replace("/", "_").replace(" ", "_").replace("(", "").replace(")", "").replace("-", "_").upper(), ext
+
 def write():
-	print(code_c())
 	with open("src/gen_assets.c", 'w', encoding='utf-8') as file:
 		file.write(code_c())
 
@@ -51,20 +64,33 @@ const char* SPRITE_PATHS[] =
 
 	sprite_c += "};"
 
+def process_sounds(list):
+	global sfx_h, sfx_c
+	sfx_h = f"#define SFX_PATH_COUNT {len(list)}\n\n"
+
+	n = 0
+	for id in list:
+		sfx_h += f"#define SFX_{id} {n} \n"
+		n += 1
+	pass
+
 def main():
 	texture_id = {}
+	sfx_id = defaultdict(list)
 
 	for path, subdirs, files in os.walk("assets"):
 		for name in files:
-			p = os.path.join(path, name)
-			id = p.replace("assets\\", "")
-			id, ext = os.path.splitext(id)
-			id = id.replace("\\", "_").replace(" ", "_").replace("(", "").replace(")", "").replace("-", "_").upper()
+			fullpath = os.path.join(path, name).replace("\\", "/")
+			id, ext = sanitize(fullpath)
+			id2, ext2 = sanitize(os.path.dirname(fullpath))
 
 			if name.endswith(".png"):
-				texture_id[id] = p.replace("\\", "/")
+				texture_id[id] = fullpath
+			elif name.endswith(".wav") or name.endswith(".ogg"):
+				sfx_id[id2].append(fullpath)
 
 	process_textures(texture_id)
+	process_sounds(sfx_id)
 
 if __name__ == "__main__":
 	main()
