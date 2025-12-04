a57b29bd399b7398c4f5b1d094ee98b0e344c145
John <johnmichaeljimenez.thecmd@gmail.com>
2025-12-04 12:13:25 +0800

    Added ui.css

--- dist/ui.js
+++ dist/ui.js
@@ -2,7 +2,7 @@ function initUI()
 {
 	const buttons = document.getElementsByClassName("ui-button");
 	for (let i = 0; i < buttons.length; i++) {
-		console.log(buttons[i].id);
+		// console.log(buttons[i].id);
 		buttons[i].onclick = function()
 		{
 			Module.ccall('OnButtonClick', null, ['string', 'int'], [buttons[i].id, 0]);
