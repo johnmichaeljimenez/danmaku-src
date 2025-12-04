e86103642d25c42d1471cdfa4589facd60079483
John <johnmichaeljimenez.thecmd@gmail.com>
2025-12-04 11:54:10 +0800

    UI setup implementation

--- src/ui.c
+++ src/ui.c
@@ -1,7 +1,6 @@
 #include "ui.h"
 
-
 EMSCRIPTEN_KEEPALIVE void OnButtonClick(const char* buttonID, int data)
 {
-	TraceLog(LOG_INFO, "CLICK: %d", data);
+	TraceLog(LOG_INFO, "ID: %s CLICK: %d", buttonID, data);
 }
